43398f49a9f1195bfda9228f7b9175eac68c8b7a
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-04-26 16:59:44 -0400

    Anadiendo querystring a los links de afiliación

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,14 @@
 console.log('\'Allo \'Allo!');
 
 $('#mailChimpModal').modal('show')
+
+var queryString = window.location.search;
+
+var linkMensual = document.getElementById('linkMensual');
+linkMensual.setAttribute('href', linkMensual.getAttribute('href') + queryString);
+
+var linkSemestral = document.getElementById('linkSemestral');
+linkSemestral.setAttribute('href', linkSemestral.getAttribute('href') + queryString);
+
+var linkAnual = document.getElementById('linkAnual');
+linkAnual.setAttribute('href', linkAnual.getAttribute('href') + queryString);
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";$("#mailChimpModal").modal("show");
+"use strict";$("#mailChimpModal").modal("show");var queryString=window.location.search,linkMensual=document.getElementById("linkMensual");linkMensual.setAttribute("href",linkMensual.getAttribute("href")+queryString);var linkSemestral=document.getElementById("linkSemestral");linkSemestral.setAttribute("href",linkSemestral.getAttribute("href")+queryString);var linkAnual=document.getElementById("linkAnual");linkAnual.setAttribute("href",linkAnual.getAttribute("href")+queryString);
